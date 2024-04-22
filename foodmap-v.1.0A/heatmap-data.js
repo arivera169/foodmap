8ecc7c6e2758ed751bf8cd3584b5a1189665be40
Amber Rivera <arivera169@unm.edu>
2024-04-21 20:20:01 -0600

    --added hexbin map

---
+++ foodmap-v.1.0A/heatmap-data.js
@@ -0,0 +1,17 @@
+// theftsHeatMapData
+var storeLocator = {
+	"max": 2500,
+	"data": [{
+		"lat": 35.1065740,
+		"lng": -106.6294404,
+		"storeName": 1
+	
+	},
+	
+	{
+		"lat": 35.1065745,
+		"lng": -106.6294410,
+		"value": 150
+	},
+]
+}
